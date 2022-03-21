a16ef5eb350504e139bb3e3b1a1ea11062e2a34e
Carlos Gabri Krizak <101573197+cGabri@users.noreply.github.com>
2022-03-21 11:36:37 -0300

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (void) {
+
+    printf("Cuchame, pone el nombre papi:");
+
+    //tipo nombre; 
+
+    char nombre [15];
+    scanf("%s",nombre);
+    printf("Bienvenido %s", nombre);
+
+    return 0;
+}
